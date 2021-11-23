380a68f301cb118def71a589ccfc8a587a62d902
Kyusik Shin <banaconda.shin@gmail.com>
2021-11-23 04:28:26 +0000

    feat: add size convert function

--- convert.py
+++ convert.py
@@ -1,4 +1,36 @@
 import re
+
+unit_map = {
+    'K': 2 ** 10,
+    'M': 2 ** 20,
+    'G': 2 ** 30,
+    'T': 2 ** 40,
+    'P': 2 ** 50,
+}
+unit_set = ''.join(unit_map.keys())
+
+def convert_human_byte_to_byte(human_byte):
+    result = re.match(f'[1-9][0-9]*[{unit_set}]*', human_byte)
+    if result == None or result.start() != 0:
+        return None
+    size = result.group()
+    result = re.match('[1-9][0-9]*', size)
+    quantity = int(result.group())
+    if result.end() < len(size):
+        unit = size[result.end():]
+        return quantity * unit_map[unit]
+    return quantity
+
+def convert_byte_to_human_byte(byte):
+    if byte < 1024:
+        return str(byte)
+
+    for unit, value in unit_map.items():
+        quatity = byte // value
+        if quatity < 1024:
+            return str(quatity) + unit
+    return None
+
 def snake_to_pascal(string):
     capital = True
     result = ""
