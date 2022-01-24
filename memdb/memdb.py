a7579625301c7f0e1f673e460746287ff412230f
Kyusik Shin <banaconda.shin@gmail.com>
2022-01-24 04:47:10 +0000

    feat: [memdb] add getOne API and hide member var

--- memdb/memdb.py
+++ memdb/memdb.py
@@ -22,25 +22,25 @@ class Data:
 
 class Table:
     def __init__(self, member_list: List[Field], key_set_list: List[list]) -> None:
-        self.member_list = member_list
-        self.member_name_set = [x.name for x in member_list]
-        self.key_info_list = []
-        self.data: list[Data] = []
-        self.index_map = {}
+        self.__member_list = member_list
+        self.__member_name_set = [x.name for x in member_list]
+        self.__key_info_list = []
+        self.__data: list[Data] = []
+        self.__index_map = {}
         for key_set in key_set_list:
             key_set_hash = get_hash(key_set)
-            self.key_info_list.append(
+            self.__key_info_list.append(
                 {"key_set": key_set, "key_set_hash": key_set_hash})
-            if key_set_hash not in self.index_map:
-                self.index_map[key_set_hash] = {}
+            if key_set_hash not in self.__index_map:
+                self.__index_map[key_set_hash] = {}
         # unique
-        self.unique_key_set = key_set_list[0]
-        self.unique_key_set_hash = get_hash(self.unique_key_set)
-        self.unique_index = self.index_map[self.unique_key_set_hash]
+        self.__unique_key_set = key_set_list[0]
+        self.__unique_key_set_hash = get_hash(self.__unique_key_set)
+        self.__unique_index = self.__index_map[self.__unique_key_set_hash]
 
     def __insert_data_to_index(self, data: Data):
-        for key_info in self.key_info_list:
-            index: dict = self.index_map[key_info["key_set_hash"]]
+        for key_info in self.__key_info_list:
+            index: dict = self.__index_map[key_info["key_set_hash"]]
 
             value_hash = self.__get_value_hash(
                 key_info["key_set"], data.key_value)
@@ -49,16 +49,16 @@ class Table:
             index[value_hash].append(data)
 
     def __delete_data_from_index(self, data: Data):
-        for key_info in self.key_info_list:
-            index: dict = self.index_map[key_info["key_set_hash"]]
+        for key_info in self.__key_info_list:
+            index: dict = self.__index_map[key_info["key_set_hash"]]
 
             value_hash = self.__get_value_hash(
                 key_info["key_set"], data.key_value)
             index[value_hash].remove(data)
 
     def __updata_data_in_index(self, data: Data, old_key_value: dict, new_key_value: dict):
-        for key_info in self.key_info_list:
-            index: dict = self.index_map[key_info["key_set_hash"]]
+        for key_info in self.__key_info_list:
+            index: dict = self.__index_map[key_info["key_set_hash"]]
 
             old_value_hash = self.__get_value_hash(
                 key_info["key_set"], old_key_value)
@@ -74,7 +74,10 @@ class Table:
     def __get_data_list(self, key_info: List[dict], key_value: dict) -> List[Data]:
         ret = []
         value_hash = self.__get_value_hash(key_info['key_set'], key_value)
-        index: dict = self.index_map[key_info['key_set_hash']]
+        index: dict = self.__index_map[key_info['key_set_hash']]
+        if value_hash not in index:
+            return ret
+
         for data in index[value_hash]:
             if key_value.items() <= data.key_value.items():
                 ret.append(data)
@@ -82,14 +85,14 @@ class Table:
 
     def __get_unique(self, key_value: dict) -> Data:
         unique_value_hash = self.__get_value_hash(
-            self.unique_key_set, key_value)
-        if unique_value_hash not in self.unique_index:
+            self.__unique_key_set, key_value)
+        if unique_value_hash not in self.__unique_index:
             return None
 
-        data_value_list: list[Data] = self.unique_index[unique_value_hash]
+        data_value_list: list[Data] = self.__unique_index[unique_value_hash]
         for data_value in data_value_list:
             found = True
-            for key in self.unique_key_set:
+            for key in self.__unique_key_set:
                 if data_value.key_value[key] != key_value[key]:
                     found = False
                     break
@@ -103,7 +106,7 @@ class Table:
 
     def insert(self, *args, **kwargs) -> bool:
         key_value = dict(kwargs)
-        if set(key_value.keys()) != set(self.member_name_set):
+        if set(key_value.keys()) != set(self.__member_name_set):
             return False
 
         unique_data = self.__get_unique(key_value)
@@ -111,14 +114,14 @@ class Table:
             return False
 
         newData = Data(key_value)
-        self.data.append(newData)
+        self.__data.append(newData)
         self.__insert_data_to_index(newData)
 
         return True
 
     def update(self, *args, **kwargs):
         key_value = dict(kwargs)
-        if set(key_value.keys()) != set(self.member_name_set):
+        if set(key_value.keys()) != set(self.__member_name_set):
             return False
 
         unique_data = self.__get_unique(key_value)
@@ -139,14 +142,14 @@ class Table:
 
         for each in data_list:
             self.__delete_data_from_index(each)
-            self.data.remove(each)
+            self.__data.remove(each)
 
         return True
 
     def get(self, *args, **kwargs) -> List[Data]:
         key_value = dict(kwargs)
         key_info: List[dict] = None
-        for each_key_info in self.key_info_list:
+        for each_key_info in self.__key_info_list:
             if set(key_value.keys()) != set(each_key_info["key_set"]):
                 continue
             key_info = each_key_info
@@ -157,8 +160,25 @@ class Table:
         data_list = self.__get_data_list(key_info, key_value)
         return data_list
 
+    def getOne(self, *args, **kwargs) -> Data:
+        key_value = dict(kwargs)
+        key_info: List[dict] = None
+        for each_key_info in self.__key_info_list:
+            if set(key_value.keys()) != set(each_key_info["key_set"]):
+                continue
+            key_info = each_key_info
+
+        if key_info == None:
+            return None
+
+        data_list = self.__get_data_list(key_info, key_value)
+        if len(data_list) == 0:
+            return None
+
+        return data_list[0]
+
     def getAll(self) -> List[Data]:
-        return self.data
+        return self.__data
 
     def test(self):
         print(self.insert(port_name='veth1', port_no=1, dpid=1))
@@ -185,21 +205,21 @@ class Table:
     def __str__(self):
         ret = ''
         padding_map = {}
-        for name in self.member_name_set:
+        for name in self.__member_name_set:
             padding = len(name)
-            for each in self.data:
+            for each in self.__data:
                 if len(str(each.key_value[name])) > padding:
                     padding = len(str(each.key_value[name]))
             padding_map[name] = padding
 
         ret += '| '
-        for name in self.member_name_set:
+        for name in self.__member_name_set:
             ret += f'{name:>{padding_map[name]}} | '
         ret += '\n'
 
-        for each in self.data:
+        for each in self.__data:
             ret += '| '
-            for name in self.member_name_set:
+            for name in self.__member_name_set:
                 ret += f'{each.key_value[name]:>{padding_map[name]}} | '
             ret += '\n'
 
